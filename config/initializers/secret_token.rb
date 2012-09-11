1f9ebea155534c0f914eac5497d694941f89bfa1
Jeremy Fisher <jeremy@fisherweb.org>
2012-09-11 14:53:49 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deathpool::Application.config.secret_token = '4b4f6d7de92c4b4c8c259068c9420ecb519949f4a504ccadae1944d4b7bdea132086048d3c95343ca712a6376f0c32d0cecb096c163b53b1b315db6dda132d7f'
